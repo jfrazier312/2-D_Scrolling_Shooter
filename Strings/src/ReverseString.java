dc3f902c35fe923d74acf0778c6dbb5c9aefb9db
jfrazier312 <frazierj312@gmail.com>
2016-08-04 20:56:04 -0400

    Update ReverseString.java

--- Strings/src/ReverseString.java
+++ Strings/src/ReverseString.java
@@ -18,12 +18,12 @@ public class ReverseString {
 	
 	public void reverseAndPrint2(String str){
 		char[] arr = new char[str.length()];
-		for(int j = 0, i = str.length()-1; i >= 0; i--, j++){
-			arr[j] = str.charAt(i);
-		}
-//		for(int i = 0; i < str.length(); i++){
-//			arr[i] = str.charAt(str.length()-1-i);
+//		for(int j = 0, i = str.length()-1; i >= 0; i--, j++){
+//			arr[j] = str.charAt(i);
 //		}
+		for(int i = 0; i < str.length(); i++){
+			arr[i] = str.charAt(str.length()-1-i);
+		}
 		System.out.println(Arrays.toString(arr));
 	}
 
