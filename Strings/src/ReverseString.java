81f03f94950b77ebc85be5495cbe895bbdf05d11
Jordan Frazier <frazierj312@gmail.com>
2016-08-04 20:47:22 -0400

    Strings: Added ReverseStrings file method

---
+++ Strings/src/ReverseString.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class ReverseString {
+	
+	
+	public static void main(String[] args){
+		ReverseString a = new ReverseString();
+		
+		a.reverseAndPrint2("localRepoWorkspace");	
+	}
+	
+	public void reverseAndPrint(String str){
+		StringBuilder build = new StringBuilder();
+		build.append(str);
+		build.reverse();
+		System.out.println(build.toString());
+	}
+	
+	public void reverseAndPrint2(String str){
+		char[] arr = new char[str.length()];
+		for(int j = 0, i = str.length()-1; i >= 0; i--, j++){
+			arr[j] = str.charAt(i);
+		}
+//		for(int i = 0; i < str.length(); i++){
+//			arr[i] = str.charAt(str.length()-1-i);
+//		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
