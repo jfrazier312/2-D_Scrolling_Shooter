41bbcc7e368fbee64a485fefd80e59d8b0155566
Jordan Frazier <frazierj312@gmail.com>
2016-08-18 10:36:41 -0400

    Replacing high scores if list is too long

--- HelloWorld/src/highScoreList/HighScoreList.java
+++ HelloWorld/src/highScoreList/HighScoreList.java
@@ -22,12 +22,17 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class HighScoreList extends Application {
-	
+
 	private BorderPane root;
 	private ListView<NameScore> view;
-//	private ListView<HashMap<String, Integer>> guy;
+	// private ListView<HashMap<String, Integer>> guy;
+
 	private ListView<String> nameView;
 	private ListView<Integer> scoreView;
+
+	ObservableList<String> nameList;
+	ObservableList<Integer> scoreList;
+
 	private TextField field;
 	private Integer randomScore;
 
@@ -39,26 +44,26 @@ public class HighScoreList extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		root = new BorderPane();
-		
+
 		field = new TextField();
 		Button okBtn = new Button("OK");
 		Button closeBtn = new Button("Close");
-		
+
 		view = new ListView<NameScore>();
 		nameView = new ListView<String>();
-//		nameView.setId("listnameView-id");
+		// nameView.setId("listnameView-id");
 		scoreView = new ListView<Integer>();
 		HBox hbox = new HBox();
 		hbox.getChildren().addAll(nameView, scoreView);
-		
+
 		root.setTop(field);
 		root.setCenter(hbox);
-		
-//		root.setCenter(nameView);
+
+		// root.setCenter(nameView);
 
 		Scene scene = new Scene(root, 380, 300);
 
-		field.setAlignment(Pos.CENTER);		
+		field.setAlignment(Pos.CENTER);
 
 		scene.setOnKeyPressed(e -> {
 			if (e.getCode() == KeyCode.ENTER) {
@@ -73,7 +78,7 @@ public class HighScoreList extends Application {
 		closeBtn.setOnAction(e -> {
 			Platform.exit();
 		});
-		
+
 		primaryStage.setTitle("New folder");
 		primaryStage.setScene(scene);
 		scene.getStylesheets().add(HighScoreList.class.getResource("HighScoreStyle.css").toExternalForm());
@@ -81,96 +86,114 @@ public class HighScoreList extends Application {
 	}
 
 	public void handleOkButtonInput() {
-		// Random method to get a high score, replaced by actual high score later
+		// Random method to get a high score, replaced by actual high score
+		// later
 		Random rand = new Random();
 		randomScore = rand.nextInt(101);
 		String nameText = field.getText();
-		
-		if (view.getItems().size() >= 3 && scoreIsHighScore()) {
-			replaceHighScoreName();
-		}
-		else {
+
+		if (view.getItems().size() < 3) {
 			addHighScoreName();
+		} else if (scoreIsHighScore()) {
+			replaceHighScoreName();
+		} else {
+			//do nothing
+			field.clear();
+			System.out.println("You suck");
 		}
-		
+
 	}
-	
+
 	public boolean scoreIsHighScore() {
-		
-		for(NameScore elem : view.getItems()) {
-			
-		}
-		
-		return false;
-	}
-	
-	public void replaceHighScoreName() {
-		
-	}
-	
-	
-	public void addHighScoreName() {
-		String text = field.getText();
-		
-		boolean isChar = true;
-		for (int i = 0; i < text.length(); i++) {
-			if(text.charAt(i) == ' ') break;
-			if (!Character.isLetter(text.charAt(i))) {
-				field.clear();
-				Alert alert = new Alert(AlertType.ERROR);
-				alert.setHeaderText("Incorrect Input");
-				alert.setContentText("Please input a string");
-				alert.showAndWait();
-				isChar = false;
-				break;
+		for (Integer elem : scoreView.getItems()) {
+			if (randomScore > elem) {
+				return true;
 			}
 		}
-		if (isChar && !view.getItems().contains(text)) {
+		return false;
+	}
+
+	public void replaceHighScoreName() {
+		String text = field.getText();
+
+		boolean isValid = checkIfValidName(text);
+
+		if (isValid) {
+			int min = 0;
+			int index = 0;
+			scoreList = scoreView.getItems();
+			for (int i = 0; i < scoreList.size(); i++) {
+				if (scoreList.get(i) < min) {
+					min = scoreList.get(i);
+					index = i;
+
+				}
+			}
 			ObservableList<NameScore> list = view.getItems();
+			list.remove(index);
 			list.add(new NameScore(text, randomScore));
 			FXCollections.sort(list);
 			FXCollections.reverse(list);
 			view.setItems(list);
 			
-			ObservableList<String> nameList = FXCollections.observableArrayList();
-			ObservableList<Integer> scoreList = FXCollections.observableArrayList();
-			
-			for(NameScore elem : view.getItems()) {
+			//should be doing a clear or remove all and instantiating in global field
+			nameList = FXCollections.observableArrayList(); 
+			scoreList = FXCollections.observableArrayList();
+
+			for (NameScore elem : view.getItems()) {
 				nameList.add(elem.getName());
 				scoreList.add(elem.getScore());
 			}
-			
+
+			nameView.setItems(nameList);
+			scoreView.setItems(scoreList);
+		}
+		
+		field.clear();
+	}
+
+	public void addHighScoreName() {
+		String text = field.getText();
+
+		boolean isValid = checkIfValidName(text);
+
+		//should probably allow duplicate names
+		if (isValid && !nameView.getItems().contains(text)) { 
+			ObservableList<NameScore> list = view.getItems();
+			list.add(new NameScore(text, randomScore));
+			FXCollections.sort(list);
+			FXCollections.reverse(list);
+			view.setItems(list);
+
+			nameList = FXCollections.observableArrayList();
+			scoreList = FXCollections.observableArrayList();
+
+			for (NameScore elem : view.getItems()) {
+				nameList.add(elem.getName());
+				scoreList.add(elem.getScore());
+			}
+
 			nameView.setItems(nameList);
 			scoreView.setItems(scoreList);
 		}
 		field.clear();
 	}
-	
-	/*
-	public void addHighScoreName() {
-		String text = field.getText();
-		boolean isChar = true;
-		for (int i = 0; i < text.length(); i++) {
-			if(text.charAt(i) == ' ') break;
-			if (!Character.isLetter(text.charAt(i))) {
-				field.clear();
+
+	public boolean checkIfValidName(String textName) {
+		if (textName.length() < 1)
+			return false;
+		for (int i = 0; i < textName.length(); i++) {
+			if (textName.charAt(i) == ' ')
+				break;
+			if (!Character.isLetter(textName.charAt(i))) {
 				Alert alert = new Alert(AlertType.ERROR);
 				alert.setHeaderText("Incorrect Input");
 				alert.setContentText("Please input a string");
 				alert.showAndWait();
-				isChar = false;
-				break;
+				return false;
 			}
 		}
-		if (isChar && !nameView.getItems().contains(text)) {
-			ObservableList<String> list = nameView.getItems();
-			list.add(text);
-			FXCollections.sort(list);
-			FXCollections.reverse(list);
-			nameView.setItems(list);
-		}
-		field.clear();
+		return true;
 	}
-	*/
 
 }
